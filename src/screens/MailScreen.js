0b2ae6c66fd6dd27bde5b93a44e59dd722857b79
vimalds15 <antovimalands@gmail.com>
2022-11-22 08:49:59 +0530

    created screens with boilerplate code

---
+++ src/screens/MailScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MailScreen = () => {
+  return (
+    <View>
+      <Text>MailScreen</Text>
+    </View>
+  )
+}
+
+export default MailScreen
+
+const styles = StyleSheet.create({})
---
+++ src/screens/SpacesScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SpacesScreen = () => {
+  return (
+    <View>
+      <Text>SpacesScreen</Text>
+    </View>
+  )
+}
+
+export default SpacesScreen
+
+const styles = StyleSheet.create({})
