fb7dfd35a4dd86d06b18d6b2a1decff4ab6cb390
vimalds15 <antovimalands@gmail.com>
2022-11-22 09:35:54 +0530

    implemented header

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,7 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View,Image } from 'react-native'
+import React,{useEffect} from 'react'
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
 
-const HomeScreen = () => {
+const HomeScreen = ({navigation}) => {
+    useEffect(()=>{
+        navigation.setOptions({
+            headerTitleAlign:"center",
+            headerLeft: () => (
+                <Image 
+                    style={{height:30,width:30,borderRadius:30,marginLeft:15}}
+                    source={{uri:"https://lh3.googleusercontent.com/ogw/AOh-ky2cWsAILwBTX3_R494N5SH1ZlXSfUd5xOx93gTJNg=s32-c-mo"}}
+                />
+            ),
+            headerTitle:()=>(
+                <FontAwesome5 name='twitter' size={25} color={'#00acee'} />
+            )
+        })
+    },[])
+
   return (
     <View>
       <Text>HomeScreen</Text>
