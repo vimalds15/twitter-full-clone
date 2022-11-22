de7c72bd0ae6a2fc031528db6219d89d323242d1
vimalds15 <antovimalands@gmail.com>
2022-11-22 09:35:35 +0530

    added screens and bottom tab navigation

--- App.js
+++ App.js
@@ -1,20 +1,91 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import HomeScreen from "./src/screens/HomeScreen";
+import SearchScreen from "./src/screens/SearchScreen";
+import SpacesScreen from "./src/screens/SpacesScreen";
+import NotificationScreen from "./src/screens/NotificationScreen";
+import MailScreen from "./src/screens/MailScreen";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
+import MaterialIcons from"react-native-vector-icons/MaterialIcons";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="home"
+        screenOptions={{
+          tabBarStyle: {
+            height: 65,
+            backgroundColor: "rgb(0,0,0)",
+            borderTopWidth: 0,
+          },
+          tabBarShowLabel: false,
+          tabBarActiveTintColor: "white",
+          headerStyle:{
+            backgroundColor:"black"
+          }
+        }}
+      >
+        <Tab.Screen
+          name="home"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome name="home" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="search"
+          component={SearchScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome name="search" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="spaces"
+          component={SpacesScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome name="microphone" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="notification"
+          component={NotificationScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome5 name="bell" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="mail"
+          component={MailScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialIcons name="mail-outline" size={size} color={color} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
