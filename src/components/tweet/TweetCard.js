1b3b214939d4091d75cdf34a06674a97dcf7a705
vimalds15 <antovimalands@gmail.com>
2022-11-23 08:58:38 +0530

    created tweet card with boilerplate code

---
+++ src/components/tweet/TweetCard.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const TweetCard = () => {
+  return (
+    <View>
+      <Text>TweetCard</Text>
+    </View>
+  )
+}
+
+export default TweetCard
+
+const styles = StyleSheet.create({})
