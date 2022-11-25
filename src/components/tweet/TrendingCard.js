048d5d347baa9fdba685596ca1006bd0765aa871
vimalds15 <antovimalands@gmail.com>
2022-11-25 09:25:36 +0530

    created TrendingCard with boilerplate

---
+++ src/components/tweet/TrendingCard.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const TrendingCard = () => {
+  return (
+    <View>
+      <Text>TrendingCard</Text>
+    </View>
+  )
+}
+
+export default TrendingCard
+
+const styles = StyleSheet.create({})
