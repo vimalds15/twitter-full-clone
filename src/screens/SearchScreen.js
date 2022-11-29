cfd29f5724d0b4bbcb7a9544885573a06a7279fa
vimalds15 <antovimalands@gmail.com>
2022-11-29 15:49:21 +0530

    mapped profile card component

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,12 +1,16 @@
 import { StyleSheet, Text, TextInput,Image, View,Dimensions, ScrollView } from 'react-native'
-import React,{useEffect} from 'react'
+import React,{useEffect,useState} from 'react'
 import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 import TrendingCard from "../components/TrendingCard"
 import { TrendingData } from '../dummydata/DummyData';
+import { DummyData } from '../dummydata/DummyData';
+import SearchProfCard from '../components/searchProfCard';
 
 const ScreenWidth = Dimensions.get('window').width;
 
 const SearchScreen = ({navigation}) => {
+  const[user,setUser] = useState([])
+  const data = DummyData
 
   useEffect(()=>(
     navigation.setOptions({
@@ -15,6 +19,7 @@ const SearchScreen = ({navigation}) => {
       placeholder='Search Twitter' 
       placeholderTextColor={"#807c7c"}
       style={styles.searchText}
+      onChangeText={searchUser}
       />),
       headerLeft: () => (
         <Image
@@ -26,18 +31,31 @@ const SearchScreen = ({navigation}) => {
     ),
     headerStyle:{
       backgroundColor:"black",
-      flex:1,
-      flexDirection:"row"
     }
     })
   ))
+  
+  const searchUser = (e) => {
+    if(e){
+      setUser(data.filter(dat => dat.id.includes(e))),
+      console.log(user)
+    }else{
+      setUser([])
+    }
+  }
 
   return (
     <View style={styles.container}>
       <ScrollView>
+      {user?.map(dat =>
+        <SearchProfCard 
+        key ={dat.id} prof={dat.prof} id={dat.id} name={dat.name} 
+        verified={dat.verified} 
+        />
+        )}
       {TrendingData.map(dat => 
         
-          <TrendingCard topic={dat.topic} hashtag={dat.hashtag} count={dat.count} />
+          <TrendingCard key={dat.id} topic={dat.topic} hashtag={dat.hashtag} count={dat.count} />
         )}
       </ScrollView>
     </View>
